edcacadfe99834392814755ebadf234d5d9187da
kshreeya2006 <kshreeya2006@gmail.com>
2025-06-03 15:39:43 +0530

    update

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import userRouter from "./routes/userRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
 
 const app = express();
-app.listen(8080,()=>{
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRouter);
+
+app.use("/products", productRouter);
+
+app.use("/orders", orderRouter);
+
+app.listen(8080, () => {
   mongoose.connect("mongodb://localhost:27017/gcet");
   console.log("Server Started");
 });
 
-const userSchema = mongoose.Schema({
-  name: {type: String},
-  email: {type: String},
-  password: {type: String},
-});
 
-const user= mongoose.model("User", userSchema);
-
-const productSchema = mongoose.Schema({
-  name: {type: String},
-  price: {type: Number},
-});
-
-const product= mongoose.model("Product", productSchema);
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res)=>{
-  /**return res.send(`<h1>Welcome to the API Index</h1>
+/**app.get("/", (req, res)=>{
+  return res.send(`<h1>Welcome to the API Index</h1>
     <ol>
       <li><a href="/greet">/greet</a></li>
       <li><a href="/name">/name</a></li>
@@ -36,7 +32,7 @@ app.get("/", (req, res)=>{
       <li><a href="/register">/register</a></li>
       <li><a href="/login">/login</a></li>
     </ol>
-  `);**/
+  `);
    res.send("Good Morning!!");
 });
 
@@ -52,23 +48,5 @@ app.get("/weather", (req, res)=>{
   res.send("29 degrees");
 });
 
-app.get("/products", async(req, res)=>{
-  const result= await product.find();
-  return res.json(result);
-});
+**/
 
-app.post("/register", async(req, res)=>{
-  const {name, email, password} = req.body;
-  const result= await user.insertOne({name, email, password});
-  return res.json(result);
-});
-
-app.post("/login", async(req, res)=>{
-  const {email, password} = req.body;
-  const result= await user.findOne({email, password});
-  if (result) {
-      return res.json(result);
-    } else {
-      return res.json({ status: "Invalid User or Password" });
-    }
-});
---
+++ routes/userRoutes.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import userModel from "../models/userModel.js";
+
+const userRouter = express.Router()
+
+userRouter.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const result = await userModel.insertOne({ name: name, email: email, password: password });
+  return res.json(result);
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const result = await userModel.findOne({ email, password });
+  if (!result) return res.json({ message: "Invalid user or password" });
+  return res.json(result);
+});
+
+userRouter.get("/:id", async(req, res)=>{
+    const email= req.params.id;
+    const result= await userModel.findOne({email});
+    return res.json(result);
+});
+
+userRouter.get("/:id/name", async(req, res)=>{
+    const email= req.params.id;
+    const result= await userModel.findOne({email},{_id:0, name:1});
+    return res.json(result);
+});
+
+export default userRouter;
