59e541df881392b937e6cd3d994faad433e4d633
kshreeya2006 <kshreeya2006@gmail.com>
2025-06-07 19:33:28 +0530

    update

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import productModel from "../models/productModel.js";
+import auth from '../middleware/auth.js';
 
 const productRouter = express.Router()
 
-productRouter.get("/all", async (req, res) => {
+productRouter.get("/all", auth,  async (req, res) => {
   const products = await productModel.find();
+  res.json(products);
+});
+
+productRouter.post("/new", async (req, res) => {
+  const product = req.body
+  const products = await productModel.create(product);
   res.json(products);
 });
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,28 @@
-import express from 'express'
+import express from 'express';
+import bcrypt from 'bcryptjs';
 import userModel from "../models/userModel.js";
 
+import jwt from "jsonwebtoken";
+const SECRET_KEY= "helloworld";
 const userRouter = express.Router()
 
 userRouter.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  const result = await userModel.insertOne({ name: name, email: email, password: password });
+  const hashpassword = await bcrypt.hash(password, 10);
+  const result = await userModel.insertOne({ name: name, email: email, password: hashpassword });
   return res.json(result);
 });
 
 userRouter.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  const result = await userModel.findOne({ email, password });
-  if (!result) return res.json({ message: "Invalid user or password" });
-  return res.json(result);
+  const result = await userModel.findOne({ email });
+  if (!result) return res.json({ message: "Invalid user" });
+  const matchPassword = await bcrypt.compare(password, result.password);
+  if(!matchPassword){
+    return res.status(400).json({message: "Invalid Password"});
+  }
+  const token = jwt.sign({email: result.email, id: result._id}, SECRET_KEY);
+  return res.json({user: result, token: token});
 });
 
 userRouter.get("/:id", async(req, res)=>{
