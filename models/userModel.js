9689d374f74e7e250148e0409d23f8fa5f003dd8
kshreeya2006 <kshreeya2006@gmail.com>
2025-06-08 12:24:19 +0530

    updated

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const userSchema = mongoose.Schema({
   name: { type: String },
   email: { type: String },
-  pass: { type: String },
+  password: { type: String },
 });
 // const user = mongoose.model("User", userSchema);
 
